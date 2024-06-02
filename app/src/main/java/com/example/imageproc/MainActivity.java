a0c104b1f7995112bfe9745db65cf0b2108b01f6
tuawei <shaclon.oao@gmail.com>
2024-06-02 17:28:22 +0800

    hello

--- app/src/main/java/com/example/imageproc/MainActivity.java
+++ app/src/main/java/com/example/imageproc/MainActivity.java
@@ -23,12 +23,15 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.android.JavaCameraView;
+import org.opencv.calib3d.Calib3d;
 import org.opencv.core.Core;
 import org.opencv.core.DMatch;
+import org.opencv.core.KeyPoint;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfDMatch;
 import org.opencv.core.MatOfKeyPoint;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.features2d.BFMatcher;
@@ -41,6 +44,7 @@ import org.opencv.features2d.Feature2D;
 import org.opencv.features2d.DescriptorMatcher;
 import org.opencv.features2d.BOWImgDescriptorExtractor;
 import org.opencv.features2d.BRISK;
+import org.opencv.utils.Converters;
 
 
 import java.io.IOException;
@@ -56,6 +60,8 @@ public class MainActivity extends CameraActivity {
     CameraBridgeViewBase.CvCameraViewFrame prevFrame;
     TextView tv;
     ImageView matResult;
+    double rotationSum;
+    //double movement;
 
     Mat curr_gray , prev_gray , dst , descriptor1 , descriptor2 ,result;
     boolean is_init;
@@ -78,6 +84,7 @@ public class MainActivity extends CameraActivity {
         cameraBridgeViewBase.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener2() {
             @Override
             public void onCameraViewStarted(int width, int height) {
+                System.out.print("hello");
                 curr_gray = new Mat();
                 prev_gray = new Mat();
 
@@ -91,6 +98,8 @@ public class MainActivity extends CameraActivity {
                 descriptor2 = new Mat();
 
                 matches = new MatOfDMatch();
+
+                rotationSum = 0;
                 //cnts = new ArrayList<>();
             }
 
@@ -112,59 +121,71 @@ public class MainActivity extends CameraActivity {
                 curr_gray = inputFrame.gray();
 
                 ORB orb = ORB.create();
-                orb.setEdgeThreshold( 70 );//設置找keypoint時圖像邊緣的閥值
-                orb.setFastThreshold( 170 );//檢測keypoint時的閥值
+                orb.setEdgeThreshold( 40 );//設置找keypoint時圖像邊緣的閥值
+                orb.setFastThreshold( 150 );//檢測keypoint時的閥值
 
                 keyPoints1.release();
                 keyPoints2.release();
-                orb.detect( curr_gray , keyPoints1 );//兩偵圖片檢測keypoint存在keypoints
-                orb.detect( prev_gray , keyPoints2 );
-
-//------------------------------------------------------------
 
                 descriptor1.release();
                 descriptor2.release();
-                orb.compute( curr_gray , keyPoints1 , descriptor1 );//計算各關鍵點的descriptor
-                orb.compute( curr_gray , keyPoints2 , descriptor2 );
-
-//------------------------------------------------------------
-
-                BFMatcher matcher = BFMatcher.create();
 
                 matches.release();
                 dst.release();
 
+                orb.detectAndCompute( prev_gray , new Mat() , keyPoints1 , descriptor1 );//尋找並計算keypoint的descriptor
+                orb.detectAndCompute( curr_gray , new Mat() , keyPoints2 , descriptor2 );
+
+                BFMatcher matcher = BFMatcher.create( Core.NORM_HAMMING , true );
                 if( descriptor1.empty() || descriptor2.empty() ){//若兩偵圖其一的關鍵點描述子為空
                     return curr_gray;
                 }else{
                     matcher.match( descriptor1 , descriptor2 , matches );//匹配descriptor 並將結果存在matches
                 }
 
-
                 if( matches.empty() ){
                     return curr_gray;
-                }else{
-
-                    runOnUiThread(new Runnable() {//這裡計算並顯示兩偵圖片中匹配到的keypoint數量 上限500
-                        @Override
-                        public void run() {
-
-                            DMatch[] matchesArray = matches.toArray();//這裡計算並顯示兩偵圖片中匹配到的keypoint數量 上限500
-                            int matchCount=matchesArray.length;
-                            String countString = "match數量:"+String.valueOf(matchCount);
-                            tv.setText(countString);
-
-                            Features2d.drawMatches( curr_gray , keyPoints1 , prev_gray , keyPoints2 , matches , result );//顯示drawmatch結果
-                            Bitmap resultBitmap = Bitmap.createBitmap(result.cols(), result.rows(), Bitmap.Config.ARGB_8888);
-                            Utils.matToBitmap(result, resultBitmap);
-                            matResult.setImageBitmap(resultBitmap);
-
-                        }
-                    });
-
                 }
 
-                Features2d.drawKeypoints( curr_gray , keyPoints1 , dst );
+                List<DMatch> matchList = matches.toList();//取前10可靠的匹配結果
+                matchList.sort( (a, b) -> Double.compare(a.distance, b.distance) );
+
+                List<DMatch> topMatches = new ArrayList<>();
+                if( matchList.size() > 20 ){
+                    topMatches =  matchList.subList(0,20);
+                }else{
+                    topMatches = matchList;
+                }
+
+                List<Point> srcPointsList = new ArrayList<>();//第一張圖的匹配點座標
+                List<Point> dstPointsList = new ArrayList<>();//第二章圖的匹配點座標
+                KeyPoint[] keypoints1Array = keyPoints1.toArray();
+                KeyPoint[] keypoints2Array = keyPoints2.toArray();
+                for (DMatch match : topMatches) {
+                    srcPointsList.add(keypoints1Array[match.queryIdx].pt);
+                    dstPointsList.add(keypoints2Array[match.trainIdx].pt);
+                }
+
+                Mat srcPoints = Converters.vector_Point2f_to_Mat(srcPointsList);
+                Mat dstPoints = Converters.vector_Point2f_to_Mat(dstPointsList);
+
+                // 計算M 旋轉平移矩陣
+                Mat M = Calib3d.estimateAffinePartial2D(srcPoints, dstPoints);
+
+                // 提取旋轉角度
+                double rotationAngle = -Math.atan2(M.get(0, 1)[0], M.get(0, 0)[0]) * 180 / Math.PI;
+                rotationSum += rotationAngle;
+
+                runOnUiThread(new Runnable() {//這裡計算並顯示兩偵圖片中匹配到的keypoint數量 上限500
+                    @Override
+                    public void run() {
+
+                        tv.setText(String.valueOf(rotationSum));
+
+                    }
+                });
+
+                Features2d.drawKeypoints( curr_gray , keyPoints2 , dst );
 
                 prev_gray = curr_gray;
 
